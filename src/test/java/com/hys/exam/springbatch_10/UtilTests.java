51cce50c7c7bdf4fa465642d440c5fdfe3cf758c
qBlackbirdp <blackbird9711@gmail.com>
2024-10-22 14:25:09 +0900

    특정 연 월에 해당하는 마지막 날짜 구하기 (테스트 케이스)

---
+++ src/test/java/com/hys/exam/springbatch_10/UtilTests.java
@@ -0,0 +1,28 @@
+package com.hys.exam.springbatch_10;
+
+import com.hys.exam.springbatch_10.util.Util;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UtilTests {
+    @Test
+    @DisplayName("특정 연 월의 마지막 날을 계산")
+    void t1(){
+        int endDayOf_2023_02 = Util.date.getEndDayOf(2023,02);
+        assertThat(endDayOf_2023_02).isEqualTo(28);
+    }
+    @Test
+    @DisplayName("특정 연 월의 마지막 날을 계산")
+    void t2(){
+        int endDayOf_2024_02 = Util.date.getEndDayOf(2024,02);
+        assertThat(endDayOf_2024_02).isEqualTo(29);
+    }
+    @Test
+    @DisplayName("특정 연 월의 마지막 날을 계산")
+    void t3(){
+        int endDayOf_2024_04 = Util.date.getEndDayOf(2024,04);
+        assertThat(endDayOf_2024_04).isEqualTo(30);
+    }
+}
